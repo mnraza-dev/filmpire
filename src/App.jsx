b3227a6b38ee14b6081eafceedaced14352acecf
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-23 09:58:45 +0530

    Refactor App component to improve structure by consolidating imports and adding Navbar. Updated routing to include MovieInformation component for enhanced user experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { CssBaseline } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Actors from './components/Actors/Actors';
-import Profile from './components/Profile/Profile';
-import Movies from "./components/Movies/Movies";
-
+import {
+  Actors,
+  Movies,
+  Profile,
+  Navbar,
+  MovieInformation,
+} from "./components";
 const App = () => {
   return (
     <div>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Movies/>} />
-          <Route path="/movie/:id" element={<h1>Hi there</h1>} />
-          <Route path="/actors/:id" element={<Actors />} />
-          <Route path="/profile/:id" element={<Profile />} />
-        </Routes>
-      </BrowserRouter>
-      <main></main>
+      <Navbar />
+
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actors/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <Box>
+        <Typography variant='h1'>Navbar</Typography>
+    </Box>
+  )
+}
+
+export default Navbar
