3af83ddd6d6336e9bec95db1005e235a30def687
Md Noorullah Raza <mnraza94@gmail.com>
2025-03-24 05:34:22 +0530

    Add authentication buttons to Navbar component with login/logout functionality. Integrate AccountCircle and Logout icons for user actions, enhancing user experience.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,9 +15,13 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 import { ToolBar } from "./styles";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Brightness7, Brightness4 } from "@mui/icons-material";
+import { useState } from "react";
+import { AccountCircle } from "@mui/icons-material";
+import { Logout } from "@mui/icons-material";
 
 const Navbar = ({ toggleColorMode }) => {
   const isMobile = useMediaQuery("(max-width:600px)");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const theme = useTheme();
   return (
     <>
@@ -36,6 +40,26 @@ const Navbar = ({ toggleColorMode }) => {
           <IconButton color="inherit" sx={{ ml: 1 }} onClick={toggleColorMode}>
             {theme.palette.mode === "dark" ? <Brightness7 /> : <Brightness4 />}
           </IconButton>
+
+          {!isMobile && "Search ..."}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button color="inherit" component={Link} to={`/profile/:id`} className="text-gray-800 " onClick={() => {}}>
+                {/* Logout &nbsp; <Logout /> */}
+
+                {!isMobile && <>Movies &nbsp; </>}
+                <Avatar style={{
+                  width:30, height:30,
+                  alt="Profile"
+                  src=""
+                }}/>
+              </Button>
+            )}
+          </div>
         </ToolBar>
       </AppBar>
     </>
